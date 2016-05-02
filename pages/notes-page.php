a5f56245cd7c58328e7f752258c7994ad90c7ba7
Daniel <slupdo09@gmail.com>
2016-05-02 23:04:52 +0300

    Basic Calendar
    Прикручен календарь

--- pages/notes-page.php
+++ pages/notes-page.php
@@ -71,6 +71,14 @@ require_once("../functions.php");
 
 </div>
 <div class="container" id="container">
+
+    <?php
+
+    ?>
+
+
+    <a class="datepicker btn-large red btn-flat white-text" value="123"><i class="material-icons left">today</i>SHOWING ALL NOTES</a>
+
     <div class="section">
         <div class="row"><!--Без этого div выстроятся в одну линию-->
             <?php
@@ -233,6 +241,22 @@ EOL;
     </div>
 </div>
 
+<script type="text/javascript">
+    $('.datepicker').pickadate({
+        labelMonthNext: 'Next month',
+        labelMonthPrev: 'Previous month',
+        labelMonthSelect: 'Select a month',
+        labelYearSelect: 'Select a year',
+        monthsFull: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ],
+        monthsShort: [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ],
+        weekdaysFull: [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ],
+        weekdaysShort: [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ],
+        weekdaysLetter: [ 'S', 'M', 'T', 'W', 'T', 'F', 'S' ],
+        today: 'Today',
+        clear: 'Clear',
+        close: 'Confirm'
+    });
+</script>
 
 </body>
 </html>
